5533219942736ee559f7ea1dde2343ab41692c7a
ChanHee Cho <ericcho0523@gmail.com>
2026-09-10 11:11:28 +0900

    p3: scores pipeline

--- p3_scores.js
+++ p3_scores.js
@@ -12,8 +12,24 @@
 const scores = Array.from({ length: 20 }, () => Math.floor(Math.random() * 101));
 console.log("start:", scores);
 
-// (a)
+// (a) 
+const odds = scores.filter(n=>n%2 !== 0);
+console.log("odds:",odds);
 
+const double = odds.map(n=>n*2);
+console.log("double:",double);
+
+const descend = double.sort((a,b) => b-a);
+console.log("descend:",descend);
+
+const half = Math.ceil(descend.length/2);
+const firsthalf = descend.slice(0,half);
+console.log("firsthalf:",firsthalf);
 // (b)
+const result = scores.filter(n=>n%2 !== 0).map(n=>n*2).sort((a,b) => b-a).slice(0,Math.ceil(scores.filter(n => n % 2 !== 0).length / 2));
+console.log("pipeline result:",result);
+
 
 // (c)
+console.log("pipeline result:",result);
+// 이유는 filter 와 map이 원본을 수정하지 않고 복사본에 배열을 생성했기 때문입니다.
