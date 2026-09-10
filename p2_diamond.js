14b2e7d02e4c6b18e29a5a3e5e393c80d562dc04
ChanHee Cho <ericcho0523@gmail.com>
2026-09-10 10:43:36 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -11,6 +11,20 @@
 
 const n = Number(process.argv[2] ?? 5);
 
+let stars = 1;
+
+for(let i=0;i<n;i++){
+    const spaces = (n - stars) / 2;
+    console.log(" ".repeat(spaces) + "*".repeat(stars));
+
+    if(i<n/2 - 1){
+        stars += 2;
+    }else{
+        stars -= 2;
+    }
+}
+
+
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
 // Hint: "*".repeat(3) gives "***".
